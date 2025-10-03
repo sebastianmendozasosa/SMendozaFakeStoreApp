620d5a98c40a3c0347700334e2d7a24886d68948
Sebastián Mendoza Sosa <sms79250@lasallebajio.edu.mx>
2025-10-03 17:18:30 -0600

    Se agrega un color extra para el ProductCard.kt, e igual se modifica este para que se vea de manera de tarjeta rectangular

--- app/src/main/java/com/example/fakestoreapp/components/ProductCard.kt
+++ app/src/main/java/com/example/fakestoreapp/components/ProductCard.kt
@@ -2,13 +2,17 @@ package com.example.fakestoreapp.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,11 +20,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.example.fakestoreapp.models.Product
 import com.example.fakestoreapp.models.Rating
+import com.example.fakestoreapp.ui.theme.CardBackground
 import com.example.fakestoreapp.ui.theme.FakeStoreAppTheme
 
 @Composable
@@ -28,6 +35,8 @@ fun ProductCard(
     product: Product,
     onClick : () -> Unit
 ) {
+
+/*
     // Implementación del ProductCard
     Row(
         modifier = Modifier
@@ -35,7 +44,7 @@ fun ProductCard(
             .fillMaxWidth()
             .height(100.dp)
             .clip(CircleShape)
-            .background(Color.Cyan)
+            .background(Color.Green)
             .padding(10.dp)
             .clickable{
               onClick()
@@ -63,6 +72,66 @@ fun ProductCard(
         Text( "$${product.price}" )
     }
 
+    */
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .width(200.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(CardBackground)
+            .clickable{
+                onClick()
+            }
+            .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = product.image,
+            contentDescription = product.title,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .background(Color.White),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // nombre
+        Text(
+            text = product.title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            maxLines = 1
+        )
+
+        //categoria
+        Text(
+            text = product.category,
+            fontSize = 12.sp,
+            color = Color.Gray
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        //precio
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.White)
+                .padding(horizontal = 10.dp, vertical = 4.dp)
+        ){
+            Text(
+                text = "$${product.price}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 12.sp,
+                color = Color.Black
+            )
+
+        }
+
+    }
 }
 
 
